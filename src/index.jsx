94eede798840cf39b175474c2f4552136c452a00
Esbjörn Blomquist <esbjorn.blomquist@cybercom.com>
2017-07-12 18:59:33 +0200

    added src, first draft

---
+++ src/index.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { List } from 'immutable';
+import IPropTypes from 'react-immutable-proptypes';
+
+const transition = 'all .4s ease';
+
+const styles = {
+  line: {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    width: '100%',
+  },
+  bar: {
+    transition,
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    height: '100%',
+  },
+};
+
+class Barz extends React.PureComponent {
+  static propTypes = {
+    width: PropTypes.number,
+    scale: PropTypes.objectOfType(PropTypes.oneOf(PropTypes.number, PropTypes.array)),
+    unit: PropTypes.string,
+    bars: IPropTypes.list,
+  };
+  static defaultProps = {
+    width: 300,
+    scale: { min: 0, max: 100 },
+    unit: '',
+    bars: List(),
+  };
+
+  // shouldComponentUpdate(nextProps) {
+  //   const { width, bars } = nextProps;
+  //   return width !== this.props.width || JSON.stringify(bars) !== JSON.stringify(this.props.bars);
+  // }
+
+  renderBar(name, value) {
+    const { width, barUnit, scale, lineStyle, barStyle, textStyle } = this.props;
+    const length = width / (scale.max - scale.min) * (value - scale.min);
+    const rgb = scale.minColor.map((c, i) =>
+      Math.floor(c + (scale.maxColor[i] - c) * length / width),
+    );
+    const color = `rgb(${rgb.join(',')})`;
+    const bgColor = `rgb(${scale.bgColor.join(',')})`;
+    const background = `linear-gradient(to right, ${color} 50%, ${bgColor} 50%, ${bgColor}) no-repeat`;
+    return (
+      <div style={{ ...lineStyle, position: 'relative', backgroundColor: bgColor }}>
+        <div style={{ ...textStyle }}>&nbsp;</div>
+        <div style={{ ...styles.bar, ...barStyle, width: length, backgroundColor: color }} />
+        <div style={{ ...styles.line, ...lineStyle }}>
+          <div style={{}}>
+            <div style={{ float: 'left', ...textStyle }}>
+              {name}
+            </div>
+            <div style={{ float: 'right', ...textStyle }}>
+              {value}
+              {barUnit}
+            </div>
+            <div style={{ clear: 'both' }} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    const { scale, barUnit, bars, width } = this.props;
+    return (
+      <div style={{ width, transition }}>
+        {bars.map(pair => this.renderBar(pair[0], pair[1]))}
+      </div>
+    );
+  }
+}
